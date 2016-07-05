ba7387e7f83715d10e74e959d7182596885ef338
yangbin <619280492@qq.com>
2016-07-05 16:19:14 +0800

    post data by curl

--- post_data_by_curl/index.php
+++ post_data_by_curl/index.php
@@ -2,7 +2,7 @@
 header('content-type:text/html;charset=utf-8');
 //初始化
 $ch = curl_init();
-$url = "http://localhost/Test/curl-post/curl_post.php";
+$url = "http://localhost/demo/post_data_by_curl/receive_postdata.php";//注意这里必须写全地址，不能写相对地址
 $post_data = array ("username" => "bob","key" => "12345");
 //设置
 curl_setopt($ch, CURLOPT_URL, $url);
