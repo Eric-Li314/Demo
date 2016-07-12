dce796eddb62b714f5f6590f14081946e9f15ab7
yangbin <619280492@qq.com>
2016-07-12 11:31:10 +0800

    优化留言板

--- message_board/admin/admin.php
+++ message_board/admin/admin.php
@@ -16,9 +16,33 @@ $pagesize = 5;
 
 $pg = new Page($total,$pagesize);
 $start = $pg->getStart();
+$where = false;
 
-$data = $db -> getAll('message','*',false,"time DESC","$start,$pagesize");
-$pagestr = $pg->showpage();
+//处理查询结果
+if(isset($_REQUEST['query'])){
+    if(!$_REQUEST['query']){
+        header('location:admin.php');
+    }else{
+        $where = "contact like '%{$_REQUEST['query']}%' or message like '%{$_REQUEST['query']}%'";
+        $total = $db->execSql("select count(*) num from message where $where");
+        $total = $total[0]['num'];
+        $pg = new Page($total,$pagesize);
+        $start = $pg->getStart();
+        $flag = 1;
+        $keyword = $_REQUEST['query'];
+    }
+}
+
+$data = $db -> getAll('message','*',$where,"time DESC","$start,$pagesize");
+$pagestr = $data ? $pg->showpage() : '没有数据！！！';
+
+if(isset($flag)){
+    foreach($data as $k=>$v){
+        $replacement = '<span style="background:greenyellow">'.$keyword.'</span>';
+        $data[$k]['contact'] = preg_replace("/$keyword/",$replacement,$v['contact']);
+        $data[$k]['message'] = preg_replace("/$keyword/",$replacement,$v['message']);
+    }
+}
 
 ?>
 
@@ -45,7 +69,11 @@ $pagestr = $pg->showpage();
 <span>当前管理员:[<?php echo $_SESSION['username'];?>]</span>
 <span style="margin-left:86%"><a href="logout.php?logout=1">注销</a></span>
 <hr/>
-<a href="../index.php">返回留言板</a>&nbsp;&nbsp;<span>（通过点击修改可以查看留言详情。）</span>
+<a href="../index.php">返回留言板</a>&nbsp;&nbsp;
+<form action="">
+    <input name="query"/>
+    <input type="submit">
+</form>
 <table border="1" style="border:1px solid green;border-collapse: collapse;font-size:14px">
     <tr>
         <th width="40">序号</th>
@@ -61,7 +89,7 @@ $pagestr = $pg->showpage();
             <td><?php echo $k+1;?></td>
             <td><?php echo $v['ip'];?></td>
             <td><?php echo $v['contact'];?></td>
-            <td><?php echo mb_substr($v['message'],0,20,'utf-8'),'...';?></td>
+            <td><?php echo $v['message'];?></td>
             <td><?php echo date('Y-m-d H:i:s',$v['time']);?></td>
             <td>
                 <?php if($v['checked']==0){?>
